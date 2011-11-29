0e29cf8bb4afbff19d14bc73427eba38325127e4
Brittany Clark <ClarkBM02@uww.edu>
2011-11-29 15:14:25 -0600

    ok

--- raildemo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Raildemo::Application.config.secret_token = '33b3300b72e56dbacb824043ff6b5c5299b21110186dc52efe620d39267902b83ebb401927109bbbfdd42a78d04ac21005556aa334cac7ba3ce1103b669885a3'
